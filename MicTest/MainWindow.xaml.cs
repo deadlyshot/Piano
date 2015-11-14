e81287e2c788e505c34a3907b1e31ea39b95fc87
Dumitru Daniel Mititelu <mttl_daniel@yahoo.com>
2015-11-14 10:11:13 +0200

    Add simple freaquency reader

--- MicTest/MainWindow.xaml.cs
+++ MicTest/MainWindow.xaml.cs
@@ -16,84 +16,85 @@ using System.Windows.Navigation;
 using System.Windows.Threading;
 using System.IO;
 using System.Threading;
+using NAudio.Dsp;
 
 namespace MicTest {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        public DispatcherTimer t=new DispatcherTimer ();
-       // private FftPitchDetector pitchDetector;
+        public DispatcherTimer t=new DispatcherTimer();
         WaveFileWriter writer;
         WaveFormat recordingFormat;
+        AudioFileReader inputStream;
         string waveFileName;
 
-        public MainWindow () {
-            InitializeComponent ();
-            // Volume();
-            Record ();
+        public MainWindow() {
+            InitializeComponent();
+            //Volume();
+            Record();
         }
 
-        private void Record () {
-            List<WaveInCapabilities> sources=new List<WaveInCapabilities> ();
-            for (int i=0; i<WaveIn.DeviceCount; i++) {
-                sources.Add (WaveIn.GetCapabilities (i));
+        private void Record() {
+            List<WaveInCapabilities> sources=new List<WaveInCapabilities>();
+            for(int i=0 ; i<WaveIn.DeviceCount ; i++) {
+                sources.Add(WaveIn.GetCapabilities(i));
             }
-            foreach (var source in sources) {
-                sourceList.Items.Add (source.ProductName);
+            foreach(var source in sources) {
+                sourceList.Items.Add(source.ProductName);
             }
         }
 
-        private void Volume () {
-            MMDeviceEnumerator enumerator=new MMDeviceEnumerator ();
-            var devices=enumerator.EnumerateAudioEndPoints (DataFlow.All, DeviceState.Active);
-            foreach (var dev in devices) {
-                comboBox.Items.Add (dev);
+        private void Volume() {
+            MMDeviceEnumerator enumerator=new MMDeviceEnumerator();
+            var devices=enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            foreach(var dev in devices) {
+                comboBox.Items.Add(dev);
             }
-            t.Interval=new TimeSpan (0, 0, 1);
+            t.Interval=new TimeSpan(0, 0, 1);
             t.Tick+=t_Tick;
-            t.Start ();
+            t.Start();
         }
 
-        void t_Tick (object sender, EventArgs e) {
-            if (comboBox.SelectedItem!=null) {
+        void t_Tick(object sender, EventArgs e) {
+            if(comboBox.SelectedItem!=null) {
                 var device=(MMDevice) comboBox.SelectedItem;
-                progressBar.Value=(int) Math.Round (device.AudioMeterInformation.MasterPeakValue*100);
+                progressBar.Value=(int) Math.Round(device.AudioMeterInformation.MasterPeakValue*100);
 
             }
         }
 
         private WaveIn waveIn=null;
-        private DirectSoundOut waveOut=null;
+        private DirectSoundOut dsOut=null;
 
-        private void Record_Click (object sender, RoutedEventArgs e) {
-            if (sourceList.SelectedItems.Count==0) return;
+        private void Record_Click(object sender, RoutedEventArgs e) {
+            if(sourceList.SelectedItems.Count==0) return;
 
             int deviceNumber=sourceList.SelectedIndex;
 
-            waveIn=new WaveIn ();
+            waveIn=new WaveIn();
             waveIn.DeviceNumber=deviceNumber;
             waveIn.DataAvailable+=OnDataAvailable;
             waveIn.RecordingStopped+=OnRecordingStopped;
-            recordingFormat=new WaveFormat (44100, WaveIn.GetCapabilities (deviceNumber).Channels);
+            recordingFormat=new WaveFormat(44100, WaveIn.GetCapabilities(deviceNumber).Channels);
             waveIn.WaveFormat=recordingFormat;
-            waveFileName=Path.Combine (Path.GetTempPath (), Guid.NewGuid ()+".wav");
+            waveFileName=Path.Combine(Path.GetTempPath(), Guid.NewGuid()+".wav");
 
-            writer=new WaveFileWriter (waveFileName, recordingFormat);
+            writer=new WaveFileWriter(waveFileName, recordingFormat);
 
-            WaveInProvider waveInProvider=new WaveInProvider (waveIn);
+            WaveInProvider waveInProvider=new WaveInProvider(waveIn);
 
-            waveOut=new DirectSoundOut ();
-            waveOut.Init (waveInProvider);
+            //dsOut=new DirectSoundOut ();
+            //dsOut.Init (waveInProvider);
 
-            waveIn.StartRecording ();
-            waveOut.Play ();
+            waveIn.StartRecording();
+            //dsOut.Play ();
         }
 
-        void OnDataAvailable (object sender, WaveInEventArgs e) {
+        void OnDataAvailable(object sender, WaveInEventArgs e) {
             byte[] buffer=e.Buffer;
             int bytesRecorded=e.BytesRecorded;
-            WriteToFile (buffer, bytesRecorded);
+            WriteToFile(buffer, bytesRecorded);
 
             //for (int index=0; index<e.BytesRecorded; index+=2) {
             //    short sample=(short) ((buffer[index+1]<<8)|
@@ -103,53 +104,54 @@ namespace MicTest {
             //}
         }
 
-        private void WriteToFile (byte[] buffer, int bytesRecorded) {
+        private void WriteToFile(byte[] buffer, int bytesRecorded) {
             long maxFileLength=this.recordingFormat.AverageBytesPerSecond*2;
 
-            //if(recordingState==RecordingState.Recording
-            //    ||recordingState==RecordingState.RequestedStop) {
-            var toWrite=(int) Math.Min (maxFileLength-writer.Length, bytesRecorded);
-            if (toWrite>0) {
-                writer.Write (buffer, 0, bytesRecorded);
+            var toWrite=(int) Math.Min(maxFileLength-writer.Length, bytesRecorded);
+            if(toWrite>0) {
+                writer.Write(buffer, 0, bytesRecorded);
             } else {
-                Stop ();
+                Stop();
             }
-            // }
+
         }
 
-        public void Stop () {
-            //if(recordingState==RecordingState.Recording) {
-            //    recordingState=RecordingState.RequestedStop;
-            waveIn.StopRecording ();
-            //}
+        public void Stop() {
+            waveIn.StopRecording();
+            Console.WriteLine("Stop recording");
         }
 
-        private void OnRecordingStopped (object sender, StoppedEventArgs e) {
-            //recordingState=RecordingState.Stopped;
-            writer.Dispose ();
-            //  Stopped(this, EventArgs.Empty);
+        private void OnRecordingStopped(object sender, StoppedEventArgs e) {
+            writer.Dispose();
         }
 
-        private void Play_Click (object sender, RoutedEventArgs e) {
-            WaveOut waveOut1=new WaveOut ();
-            WaveStream source=new WaveFileReader (waveFileName);
+        public event EventHandler<FftEventArgs> FftCalculated;
 
-            if (waveOut!=null) {
-                waveOut.Stop ();
-                waveOut.Dispose ();
-                waveOut=null;
+        protected virtual void OnFftCalculated(FftEventArgs e) {
+            EventHandler<FftEventArgs> handler=FftCalculated;
+            if(handler!=null) handler(this, e);
+            showFreaquency(e.Result);
+        }
+
+        private void showFreaquency(Complex[] complex) {
+            List<double> buffer=new List<double>();
+            for(int i=0 ; i<complex.Length/2 ; i+=2) {
+                buffer.Add(Math.Sqrt(complex[i].X*complex[i].X+complex[i].Y*complex[i].Y));
             }
-            //if(waveIn!=null) {
-            //    waveIn.StopRecording();
-            //    waveIn.Dispose();
-            //    waveIn=null;
-            //}
-
-            string tempFile = Path.Combine (Path.GetTempPath (), Guid.NewGuid ()+".wav");
+            Console.WriteLine(buffer.Max()+" "+( buffer.IndexOf(buffer.Max())*inputStream.WaveFormat.SampleRate/2 )/( complex.Length/2 ));
+        }
 
 
-            waveOut1.Init (source);
-            waveOut1.Play ();
+        private void Play_Click(object sender, RoutedEventArgs e) {
+            WaveOut waveOut=new WaveOut();
+
+            inputStream=new AudioFileReader(waveFileName);
+            var aggregator=new SampleAggregator(inputStream);
+            aggregator.NotificationCount=inputStream.WaveFormat.SampleRate/100;
+            aggregator.PerformFFT=true;
+            aggregator.FftCalculated+=(s, a) => OnFftCalculated(a);
+            waveOut.Init(aggregator);
+            waveOut.Play();
         }
     }
 }
